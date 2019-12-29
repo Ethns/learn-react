022117e4ff293ed29fcf9fda8771052006b6db75
Ethan Shen <shenye601@hotmail.com>
2019-12-29 22:40:51 +1000

    map through the todos list with passing down the state object from app.js to todoItem.js as a properties/attribute of a JSX element(html element

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Welcome to React world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi Ethan
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component{
+  state = {
+    todos: [
+        {
+            id: 1,
+            title: 'task 001',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'task 002',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'task 003',
+            completed: false
+        }
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
